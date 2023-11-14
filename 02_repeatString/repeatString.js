d49ddf962dfeec7d23b2380988ef2a7ff5a0ab83
ruben-vl <rubenvanlaer@mail.com>
2023-11-14 18:38:24 +0100

    Add solution exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) return "ERROR"
+    result = ""
+    for (i = 0; i < times; i++) {
+        result = result.concat(string)
+    }
+    return result
 };
 
 // Do not edit below this line
