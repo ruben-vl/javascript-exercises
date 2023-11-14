d346a5149074315b96ef51a35d9551ca79341288
ruben-vl <rubenvanlaer@mail.com>
2023-11-14 19:09:09 +0100

    Add solution exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) {
+        return "ERROR";
+    }
+    let divBy4 = year % 4 == 0;
+    let divBy100 = year % 100 == 0;
+    let divBy400 = year % 400 == 0;
 
+    return divBy400 || (divBy4 && !divBy100);
 };
 
 // Do not edit below this line
