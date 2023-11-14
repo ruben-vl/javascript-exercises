b769a516ccb8de2f738a9ad0e0a68f46104ecb4c
ruben-vl <rubenvanlaer@mail.com>
2023-11-14 18:57:57 +0100

    Add solution exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    switch (arguments.length) {
+        case 0:
+            return [];
+        case 1:
+            return arr;
+        default:
+            for (let elem of toRemove) {
+                arr = arr.filter((e) => e !== elem)
+            }
+            return arr;
+    }
 };
 
 // Do not edit below this line
